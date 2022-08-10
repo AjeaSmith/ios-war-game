b2e329b11c7f52d198d8534412ad350ded6b60f3
ajea <smithajea@rocketmail.com>
2022-08-10 01:56:19 -0400

    War Game project complete

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -8,9 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuPlayer = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("background").edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuPlayer)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    // Genrate random number between 2-14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // update cards
+                    playerCard = "card\(playerRand)"
+                    cpuPlayer = "card\(cpuRand)"
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }else if (cpuRand > playerRand) {
+                        cpuScore += 1
+                    }
+                   
+                }, label: {
+                    Image("dealbutton")
+                })
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player").padding(.bottom, 10.0).font(.headline)
+                        Text(String(playerScore)).font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU").padding(.bottom, 10.0).font(.headline)
+                        Text(String(cpuScore)).font(.largeTitle)
+                    }
+                    Spacer()
+                }.foregroundColor(.white).font(.largeTitle)
+                Spacer()
+            }
+        }
     }
 }
 
